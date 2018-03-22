6b6c4932db56599926197d649f1d399d2b058513
Christian Martínez <christt105@gmail.com>
2018-03-22 20:53:30 +0100

    module audio initialized

---
+++ SDL_AndroDunos/ModuleAudio.cpp
@@ -0,0 +1,27 @@
+#include "Globals.h"
+#include "Application.h"
+#include "ModuleAudio.h"
+#include "SDL/include/SDL.h"
+
+#include "SDL_mixer/include/SDL_mixer.h"
+#pragma comment(lib, "SDL_mixer/libx86/SDL2_mixer.lib")
+
+ModuleAudio::ModuleAudio() :Module() {
+
+}
+
+//Destructor
+ModuleAudio::~ModuleAudio() {}
+
+//Initialization
+bool ModuleAudio::Init() {
+
+}
+
+update_status ModuleAudio::Update() {
+
+}
+
+bool ModuleAudio::CleanUp() {
+
+}
--- SDL_AndroDunos/ModuleAudio.h
+++ SDL_AndroDunos/ModuleAudio.h
@@ -3,6 +3,12 @@
 #include "Module.h"
 
 class ModuleAudio :public Module {
+public:
+	ModuleAudio();
+	~ModuleAudio();
 
+	bool Init();
+	update_status Update();
+	bool CleanUp();
 };
 #endif // !_MODULEAUDIO_H
