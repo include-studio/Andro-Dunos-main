41a63b690fe8f6834a78a04648cf11fbd3952fc5
Christian Martínez <christt105@gmail.com>
2018-03-22 21:14:25 +0100

    SDL_Mixer init and clean up

--- SDL_AndroDunos/ModuleAudio.cpp
+++ SDL_AndroDunos/ModuleAudio.cpp
@@ -15,13 +15,18 @@ ModuleAudio::~ModuleAudio() {}
 
 //Initialization
 bool ModuleAudio::Init() {
-
+	bool ret = true;
+	int init;
+	init = Mix_Init(MIX_INIT_OGG);
+	return ret;
 }
 
 update_status ModuleAudio::Update() {
-
+	return update_status::UPDATE_CONTINUE;
 }
 
 bool ModuleAudio::CleanUp() {
-
+	bool ret = true;
+	SDL_Quit();
+	return ret;
 }
