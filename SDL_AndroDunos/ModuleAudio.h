796f9cde2e7e25984061286497b0fec00e66be32
Marc Gálvez <36281630+optus23@users.noreply.github.com>
2018-03-30 01:44:32 +0200

    Added ModuleAudio.h

--- SDL_AndroDunos/ModuleAudio.h
+++ SDL_AndroDunos/ModuleAudio.h
@@ -4,11 +4,11 @@
 #include "Module.h"
 #include "Globals.h"
 
-#define MAX_BGM 50
+#define MAX_BGMS 50
 #define MAX_FXS 50
 
-//struct _Mix_Music;
-//struct Mix_Chunk;
+struct _Mix_Music;
+struct Mix_Chunk;
 
 class ModuleAudio : public Module
 {
@@ -22,11 +22,14 @@ public:
 	update_status PreUpdate();
 	bool CleanUp();
 
-	/*_Mix_Music* const LoadMusic(const char* path);
-	Mix_Chunk* const LoadFx(const char* path);*/
+	_Mix_Music* const LoadMusic(const char* path);
+	Mix_Chunk* const LoadFx(const char* path);
 
 public:
+	_Mix_Music *bgms[MAX_BGMS];
 	_Mix_Music *bgm;
+	Mix_Chunk *fxs[MAX_FXS];
+	Mix_Chunk *fx;
 };
 
 #endif //__ModuleRenderer_H__
