860a2e180acf23f28032dcc89007964b3abe8189
Christian Martínez <christt105@gmail.com>
2018-04-01 12:11:09 +0200

    Screen width fix

--- SDL_AndroDunos/Globals.h
+++ SDL_AndroDunos/Globals.h
@@ -19,7 +19,7 @@ typedef unsigned int uint;
 
 // Configuration -----------
 #define SCREEN_SIZE 3
-#define SCREEN_WIDTH 384
+#define SCREEN_WIDTH 320
 #define SCREEN_HEIGHT 224
 #define WIN_FULLSCREEN 0
 #define WIN_FULLSCREEN_DESKTOP 0
