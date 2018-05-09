7f2aeed9f6ef4a95cb55fbaa97bbe4871fc45f27
Christian Martínez <christt105@gmail.com>
2018-05-09 12:34:03 +0200

    cases stage4

--- SDL_AndroDunos/ModuleStage4.cpp
+++ SDL_AndroDunos/ModuleStage4.cpp
@@ -65,69 +65,69 @@ bool ModuleStage4::Start() {
 	ground_tx = App->textures->Load("assets/Sprites/ground4.png");
 	//colliders
 
-	App->collision->AddCollider({ 1000,215,125,100 }, COLLIDER_WALL); //x y w h
-	App->collision->AddCollider({ 1240,215,125,100 }, COLLIDER_WALL);
-	App->collision->AddCollider({ 1365,200,125,100 }, COLLIDER_WALL);
-	App->collision->AddCollider({ 1490,175,175,100 }, COLLIDER_WALL);
-	App->collision->AddCollider({ 1665,190,80,100 }, COLLIDER_WALL);
-	App->collision->AddCollider({ 1745,200,175,100 }, COLLIDER_WALL);
-	App->collision->AddCollider({ 1920,185,135,100 }, COLLIDER_WALL);
-	App->collision->AddCollider({ 2055,170,64,100 }, COLLIDER_WALL);
-	App->collision->AddCollider({ 2119,155,65,100 }, COLLIDER_WALL);
-	App->collision->AddCollider({ 2184,140,65,100 }, COLLIDER_WALL);
-	App->collision->AddCollider({ 2249,125,95,100 }, COLLIDER_WALL);
-	App->collision->AddCollider({ 2344,145,110,100 }, COLLIDER_WALL);
-	App->collision->AddCollider({ 2454,125,160,100 }, COLLIDER_WALL);
-	App->collision->AddCollider({ 2614,110,225,100 }, COLLIDER_WALL);
-	App->collision->AddCollider({ 2839,80,80,100 }, COLLIDER_WALL);
-	App->collision->AddCollider({ 2919,45,45,100 }, COLLIDER_WALL);
-	App->collision->AddCollider({ 2964,85,255,100 }, COLLIDER_WALL);
-	App->collision->AddCollider({ 3219,122,127,100 }, COLLIDER_WALL);
-	App->collision->AddCollider({ 3346,142,65,100 }, COLLIDER_WALL);
-	App->collision->AddCollider({ 3411,157,15,100 }, COLLIDER_WALL);
-	App->collision->AddCollider({ 3426,188,33,100 }, COLLIDER_WALL);
-	App->collision->AddCollider({ 3459,203,15,100 }, COLLIDER_WALL);
-	App->collision->AddCollider({ 3474,233,80,100 }, COLLIDER_WALL);
-	App->collision->AddCollider({ 3554,263,30,100 }, COLLIDER_WALL);
-	App->collision->AddCollider({ 3584,288,30,100 }, COLLIDER_WALL);
-	App->collision->AddCollider({ 3614,308,90,100 }, COLLIDER_WALL);
-	App->collision->AddCollider({ 3704,323,1530,100 }, COLLIDER_WALL);
-	App->collision->AddCollider({ 5234,313,275,100 }, COLLIDER_WALL);
-	App->collision->AddCollider({ 5509,288,25,100 }, COLLIDER_WALL);
-	App->collision->AddCollider({ 5534,268,50,100 }, COLLIDER_WALL);
-	App->collision->AddCollider({ 5584,328,225,100 }, COLLIDER_WALL);
-	App->collision->AddCollider({ 6065,328,508,100 }, COLLIDER_WALL);
-	App->collision->AddCollider({ 6573,308,130,100 }, COLLIDER_WALL);
-	App->collision->AddCollider({ 6703,298,20,100 }, COLLIDER_WALL);
-	App->collision->AddCollider({ 6723,278,20,100 }, COLLIDER_WALL);
-	App->collision->AddCollider({ 6743,285,90,100 }, COLLIDER_WALL);
-	App->collision->AddCollider({ 6833,265,125,100 }, COLLIDER_WALL);
-	App->collision->AddCollider({ 6958,250,62,100 }, COLLIDER_WALL);
-	App->collision->AddCollider({ 7020,280,80,100 }, COLLIDER_WALL);
-	App->collision->AddCollider({ 7100,310,80,100 }, COLLIDER_WALL);
-	App->collision->AddCollider({ 7180,290,90,100 }, COLLIDER_WALL);
-	App->collision->AddCollider({ 7270,270,100,100 }, COLLIDER_WALL);
-	App->collision->AddCollider({ 7370,205,65,100 }, COLLIDER_WALL);
-	App->collision->AddCollider({ 7435,160,285,100 }, COLLIDER_WALL);
-	App->collision->AddCollider({ 7720,200,210,100 }, COLLIDER_WALL);
-	App->collision->AddCollider({ 7930,235,500,100 }, COLLIDER_WALL);
+	App->collision->AddCollider({ 1000,215,125,100 }, COLLIDER_WALL4); //x y w h
+	App->collision->AddCollider({ 1240,215,125,100 }, COLLIDER_WALL4);
+	App->collision->AddCollider({ 1365,200,125,100 }, COLLIDER_WALL4);
+	App->collision->AddCollider({ 1490,175,175,100 }, COLLIDER_WALL4);
+	App->collision->AddCollider({ 1665,190,80,100 }, COLLIDER_WALL4);
+	App->collision->AddCollider({ 1745,200,175,100 }, COLLIDER_WALL4);
+	App->collision->AddCollider({ 1920,185,135,100 }, COLLIDER_WALL4);
+	App->collision->AddCollider({ 2055,170,64,100 }, COLLIDER_WALL4);
+	App->collision->AddCollider({ 2119,155,65,100 }, COLLIDER_WALL4);
+	App->collision->AddCollider({ 2184,140,65,100 }, COLLIDER_WALL4);
+	App->collision->AddCollider({ 2249,125,95,100 }, COLLIDER_WALL4);
+	App->collision->AddCollider({ 2344,145,110,100 }, COLLIDER_WALL4);
+	App->collision->AddCollider({ 2454,125,160,100 }, COLLIDER_WALL4);
+	App->collision->AddCollider({ 2614,110,225,100 }, COLLIDER_WALL4);
+	App->collision->AddCollider({ 2839,80,80,100 }, COLLIDER_WALL4);
+	App->collision->AddCollider({ 2919,45,45,100 }, COLLIDER_WALL4);
+	App->collision->AddCollider({ 2964,85,255,100 }, COLLIDER_WALL4);
+	App->collision->AddCollider({ 3219,122,127,100 }, COLLIDER_WALL4);
+	App->collision->AddCollider({ 3346,142,65,100 }, COLLIDER_WALL4);
+	App->collision->AddCollider({ 3411,157,15,100 }, COLLIDER_WALL4);
+	App->collision->AddCollider({ 3426,188,33,100 }, COLLIDER_WALL4);
+	App->collision->AddCollider({ 3459,203,15,100 }, COLLIDER_WALL4);
+	App->collision->AddCollider({ 3474,233,80,100 }, COLLIDER_WALL4);
+	App->collision->AddCollider({ 3554,263,30,100 }, COLLIDER_WALL4);
+	App->collision->AddCollider({ 3584,288,30,100 }, COLLIDER_WALL4);
+	App->collision->AddCollider({ 3614,308,90,100 }, COLLIDER_WALL4);
+	App->collision->AddCollider({ 3704,323,1530,100 }, COLLIDER_WALL4);
+	App->collision->AddCollider({ 5234,313,275,100 }, COLLIDER_WALL4);
+	App->collision->AddCollider({ 5509,288,25,100 }, COLLIDER_WALL4);
+	App->collision->AddCollider({ 5534,268,50,100 }, COLLIDER_WALL4);
+	App->collision->AddCollider({ 5584,328,225,100 }, COLLIDER_WALL4);
+	App->collision->AddCollider({ 6065,328,508,100 }, COLLIDER_WALL4);
+	App->collision->AddCollider({ 6573,308,130,100 }, COLLIDER_WALL4);
+	App->collision->AddCollider({ 6703,298,20,100 }, COLLIDER_WALL4);
+	App->collision->AddCollider({ 6723,278,20,100 }, COLLIDER_WALL4);
+	App->collision->AddCollider({ 6743,285,90,100 }, COLLIDER_WALL4);
+	App->collision->AddCollider({ 6833,265,125,100 }, COLLIDER_WALL4);
+	App->collision->AddCollider({ 6958,250,62,100 }, COLLIDER_WALL4);
+	App->collision->AddCollider({ 7020,280,80,100 }, COLLIDER_WALL4);
+	App->collision->AddCollider({ 7100,310,80,100 }, COLLIDER_WALL4);
+	App->collision->AddCollider({ 7180,290,90,100 }, COLLIDER_WALL4);
+	App->collision->AddCollider({ 7270,270,100,100 }, COLLIDER_WALL4);
+	App->collision->AddCollider({ 7370,205,65,100 }, COLLIDER_WALL4);
+	App->collision->AddCollider({ 7435,160,285,100 }, COLLIDER_WALL4);
+	App->collision->AddCollider({ 7720,200,210,100 }, COLLIDER_WALL4);
+	App->collision->AddCollider({ 7930,235,500,100 }, COLLIDER_WALL4);
 
 
-	App->collision->AddCollider({ 1645,-40,545,50 }, COLLIDER_WALL); //Up
-	App->collision->AddCollider({ 2190,-60,65,50 }, COLLIDER_WALL);
-	App->collision->AddCollider({ 2255,-110,295,50 }, COLLIDER_WALL);
-	App->collision->AddCollider({ 2550,-90,195,50 }, COLLIDER_WALL);
-	App->collision->AddCollider({ 2745,-130,100,50 }, COLLIDER_WALL);
+	App->collision->AddCollider({ 1645,-40,545,50 }, COLLIDER_WALL4); //Up
+	App->collision->AddCollider({ 2190,-60,65,50 }, COLLIDER_WALL4);
+	App->collision->AddCollider({ 2255,-110,295,50 }, COLLIDER_WALL4);
+	App->collision->AddCollider({ 2550,-90,195,50 }, COLLIDER_WALL4);
+	App->collision->AddCollider({ 2745,-130,100,50 }, COLLIDER_WALL4);
 
-	App->collision->AddCollider({ 5585,90,50,50 }, COLLIDER_WALL); //Up 2
-	App->collision->AddCollider({ 5635,80,160,100 }, COLLIDER_WALL);  
-	App->collision->AddCollider({ 5795,95,210,50 }, COLLIDER_WALL);
+	App->collision->AddCollider({ 5585,90,50,50 }, COLLIDER_WALL4); //Up 2
+	App->collision->AddCollider({ 5635,80,160,100 }, COLLIDER_WALL4);  
+	App->collision->AddCollider({ 5795,95,210,50 }, COLLIDER_WALL4);
 
-	App->collision->AddCollider({ 7070,90,20,50 }, COLLIDER_WALL); //Up3
-	App->collision->AddCollider({ 7090,80,222,100 }, COLLIDER_WALL);
-	App->collision->AddCollider({ 7312,20,35,100 }, COLLIDER_WALL);
-	App->collision->AddCollider({ 7347,-40,65,100 }, COLLIDER_WALL);
-	App->collision->AddCollider({ 7412,-65,145,100 }, COLLIDER_WALL);
+	App->collision->AddCollider({ 7070,90,20,50 }, COLLIDER_WALL4); //Up3
+	App->collision->AddCollider({ 7090,80,222,100 }, COLLIDER_WALL4);
+	App->collision->AddCollider({ 7312,20,35,100 }, COLLIDER_WALL4);
+	App->collision->AddCollider({ 7347,-40,65,100 }, COLLIDER_WALL4);
+	App->collision->AddCollider({ 7412,-65,145,100 }, COLLIDER_WALL4);
 
 
 	//reset variables (camera position, players position...)
@@ -171,12 +171,12 @@ update_status ModuleStage4::Update() {
 	//input
 
 	//logic
-	/*switch (stage) {
+	App->player1->position.x++;
+	switch (stage) {
 	case 0:				//scroll diagonal to y=43
 		App->render->camera.x += 1 * SCREEN_SIZE;
 		App->render->camera.y += 1;
 		App->render->Blit(back_tx, 0, 0, NULL, BACKGROUND4SPEED);
-		
 
 		if (App->render->camera.y >= 43*SCREEN_SIZE)
 			stage++;;
@@ -186,9 +186,9 @@ update_status ModuleStage4::Update() {
 
 		for (int i = 0; i < 3; i++)
 			App->render->Blit(back_tx, back.w*i, 0, NULL, BACKGROUND4SPEED);
-		App->render->Blit(ground_tx, 1000, -58, &ground[0], GROUND4SPEED);
+		App->render->Blit(ground_tx, 1000, -61, &ground[0], GROUND4SPEED);
 
-		if (App->render->camera.x >= 1950 * SCREEN_SIZE)
+		if (App->render->camera.x >= 2445 * SCREEN_SIZE)
 			stage++;;
 		break;
 	case 2:
@@ -197,9 +197,9 @@ update_status ModuleStage4::Update() {
 
 		for (int i = 0; i < 3; i++)
 			App->render->Blit(back_tx, back.w*i, 0, NULL, BACKGROUND4SPEED);
-		App->render->Blit(ground_tx, 1000, -52, &ground[0], GROUND4SPEED);
+		App->render->Blit(ground_tx, 1000, -61, &ground[0], GROUND4SPEED);
 
-		if (App->render->camera.y <= -156)
+		if (App->render->camera.y <= -76*SCREEN_SIZE)
 			stage++;
 		break;
 	case 3:
@@ -207,39 +207,70 @@ update_status ModuleStage4::Update() {
 
 		for (int i = 0; i < 3; i++)
 			App->render->Blit(back_tx, back.w*i, 0, NULL, BACKGROUND4SPEED);
-		App->render->Blit(ground_tx, 1000, -52, &ground[0], GROUND4SPEED);
+		App->render->Blit(ground_tx, 1000, -61, &ground[0], GROUND4SPEED);
+		App->render->Blit(ground_tx, 1000 + ground[0].w, 20, &ground[1], GROUND4SPEED);
 
+		if (App->render->camera.x >= 3880 * SCREEN_SIZE)
+			stage++;
+		break;
+	case 4:
+		App->render->camera.x += 1 * SCREEN_SIZE;
+		App->render->camera.y += 1;
 
-	}*/
-	
-	//case 2 scroll diagonal up right
-	//case 3 scroll horizontal
-		//wall brakable
-	//case 4 scroll diagonal down
-	//case 5 scroll horizontal water + big boss
-	//case 6 scroll diagonal up
-	//case 7 scroll horizontal
-	//case 8 scroll horizontal lower until stop
-	//case 9 camera no move, final boss
+		for (int i = 0; i < 3; i++)
+			App->render->Blit(back_tx, back.w*i, 0, NULL, BACKGROUND4SPEED);
+		App->render->Blit(ground_tx, 1000, -61, &ground[0], GROUND4SPEED);
+		App->render->Blit(ground_tx, 1000 + ground[0].w, 20, &ground[1], GROUND4SPEED);
+
+		if (App->render->camera.y >= 143 * SCREEN_SIZE)
+			stage++;
+		break;
+	case 5:
+		App->render->camera.x += 1 * SCREEN_SIZE;
+
+		for (int i = 0; i < 4; i++)
+			App->render->Blit(back_tx, back.w*i, 0, NULL, BACKGROUND4SPEED);
+		App->render->Blit(ground_tx, 1000 + ground[0].w, 20, &ground[1], GROUND4SPEED);
+		App->render->Blit(ground_tx, 2511 + ground[0].w + ground[1].w, 54, &ground[2], GROUND4SPEED);
+		App->render->Blit(ground_tx, 2511 + ground[0].w + ground[1].w + ground[2].w, 15, &ground[3], GROUND4SPEED);
+
+		if (App->render->camera.x >= 8952 * SCREEN_SIZE)
+			stage++;
+		break;
+	case 6:
+		App->render->camera.x += 1 * SCREEN_SIZE;
+		App->render->camera.y--;
+
+		for (int i = 0; i < 4; i++)
+			App->render->Blit(back_tx, back.w*i, 0, NULL, BACKGROUND4SPEED);
+		App->render->Blit(ground_tx, 2511 + ground[0].w + ground[1].w, 54, &ground[2], GROUND4SPEED);
+		App->render->Blit(ground_tx, 2511 + ground[0].w + ground[1].w + ground[2].w, 15, &ground[3], GROUND4SPEED);
+
+		if (App->render->camera.y <= 18 * SCREEN_SIZE)
+			stage++;
+		break;
+	case 7:
+		App->render->camera.x += 1 * SCREEN_SIZE;
+
+		for (int i = 0; i < 4; i++)
+			App->render->Blit(back_tx, back.w*i, 0, NULL, BACKGROUND4SPEED);
+		App->render->Blit(ground_tx, 2511 + ground[0].w + ground[1].w + ground[2].w, 15, &ground[3], GROUND4SPEED);
+
+		if (App->render->camera.x >= 9976 * SCREEN_SIZE)
+			stage++;
+		break;
+	case 8:
+		for (int i = 2; i < 4; i++)
+			App->render->Blit(back_tx, back.w * i, 0, NULL, BACKGROUND4SPEED);
+		App->render->Blit(ground_tx, 2511 + ground[0].w + ground[1].w + ground[2].w, 15, &ground[3], GROUND4SPEED);
+		break;
+	}
 
 	animation_water = &water;
 	water_rect = animation_water->GetCurrentFrame();
-
-	for (int i = 0; i < 5; i++)
-		App->render->Blit(back_tx, back.w*i, 0, NULL, BACKGROUND4SPEED);
 		
 	for (int i = 0; i < 5; i++)
 		App->render->Blit(ground_tx, (1000 + ground[0].w + ground[1].w) + water_rect.w*i-9, 318, &water_rect, GROUND4SPEED);
-
-	App->render->Blit(ground_tx, 1000, -95, &ground[0], GROUND4SPEED);
-	App->render->Blit(ground_tx, 1000 + ground[0].w, 20, &ground[1], GROUND4SPEED);
-	App->render->Blit(ground_tx, 2511 + ground[0].w + ground[1].w, 54, &ground[2], GROUND4SPEED);
-	App->render->Blit(ground_tx, 2511 + ground[0].w + ground[1].w  + ground[2].w, 15, &ground[3], GROUND4SPEED);
-	App->render->camera.x += 1 * SCREEN_SIZE;
-	App->player1->position.x++;
-
-	//draw everithing
-	
 
 	return UPDATE_CONTINUE;
 }
