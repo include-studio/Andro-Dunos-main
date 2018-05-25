5d1a64d7de43023a9eb5afef4c8a0adc0cbc4f90
Marc Gálvez <36281630+optus23@users.noreply.github.com>
2018-05-25 11:35:59 +0200

    Module triplecanonship created

---
+++ SDL_AndroDunos/Enemy_TripleCanon.h
@@ -0,0 +1,35 @@
+#ifndef __ENEMY_TRIPLECANON_H__
+#define __ENEMY_TRIPLECANON_H__
+
+#include "Enemy.h"
+#include "path.h"
+
+class Enemy_TripleCanon : public Enemy
+{
+private:
+
+	Path path;
+
+	int original_y = 0;
+	Animation fly;
+
+public:
+
+	Enemy_TripleCanon(int x, int y);
+	void OnCollision(Collider* collider) override;
+
+	void Move();
+
+	void Shoot();
+	/*int shot_limit = 20;
+	int shot_actual = 0;*/
+	int current_time;
+	int init_time = 0;
+	bool shoot = false;
+	
+
+	int hp = 6;
+};
+
+#endif // __ENEMY_TRIPLECANON_H__
+#pragma once
